816859d1ba901a2de241cd763af3b9790ea1978f
thorben muhleder <thorbenm@me.com>
2023-07-31 12:19:56 +0200

    add rain graph

--- update.py
+++ update.py
@@ -11,20 +11,29 @@ FONT_PATH = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
 
 
 def get_yr():
-    yr_full = capture_screenshot("https://www.yr.no/en/forecast/daily-table/2-2670879/Sweden/Stockholm/Sundbyberg%20Municipality/Sundbyberg")
-    _waveshare.save_image_to_disk(yr_full, "yr_full")
+    full = capture_screenshot("https://www.yr.no/en/forecast/daily-table/2-2670879/Sweden/Stockholm/Sundbyberg%20Municipality/Sundbyberg")
+    _waveshare.save_image_to_disk(full, "yr_full")
 
-    yr_current_conditions = yr_full.crop((330, 270, yr_full.width - 1000, yr_full.height - 750))
-    arr = np.array(yr_current_conditions).astype(float)
-    arr -= np.min(arr)
-    arr -= 100
-    arr /= np.max(arr)
-    arr *= 255.0
-    arr = np.clip(arr, 0.0, 255.0)
-    yr_current_conditions = Image.fromarray(arr.astype(int))
-    _waveshare.save_image_to_disk(yr_current_conditions, "yr_current_conditions")
+    current_conditions = full.crop((330, 270, full.width - 1000, full.height - 750))
+    current_conditions_arr = np.array(current_conditions).astype(float)
+    current_conditions_arr -= np.min(current_conditions_arr)
+    current_conditions_arr -= 100
+    current_conditions_arr /= np.max(current_conditions_arr)
+    current_conditions_arr *= 255.0
+    current_conditions_arr = np.clip(current_conditions_arr, 0.0, 255.0)
+    current_conditions = Image.fromarray(current_conditions_arr.astype(int))
+    _waveshare.save_image_to_disk(current_conditions, "yr_current_conditions")
 
-    return yr_current_conditions
+    rain_graph = full.crop((1210, 250, full.width - 360, full.height - 740))
+    rain_graph_arr = np.array(rain_graph).astype(float)
+    rain_graph_arr -= np.min(rain_graph_arr)
+    rain_graph_arr -= 25
+    rain_graph_arr /= np.max(rain_graph_arr)
+    rain_graph_arr *= 255.0
+    rain_graph_arr = np.clip(rain_graph_arr, 0.0, 255.0)
+    rain_graph = Image.fromarray(rain_graph_arr.astype(int))
+    _waveshare.save_image_to_disk(rain_graph, "yr_rain_graph")
+    return current_conditions, rain_graph
 
 
 def add_text_to_image(draw, text, size, location):
@@ -50,10 +59,14 @@ def main():
     current_time = time.strftime('%-I:%M')
     add_text_to_image(draw, current_time, 150, (-1, 20))
 
-    yr = get_yr()
-    yr = yr.resize((480, round(yr.size[1] * 480 / yr.size[0])))
+    current_conditions, rain_graph = get_yr()
 
-    image.paste(yr, (0, 200))
+    current_conditions = current_conditions.resize((480, round(current_conditions.size[1] * 480 / current_conditions.size[0])))
+    image.paste(current_conditions, (0, 200))
+
+    rain_graph = rain_graph.resize((480, round(rain_graph.size[1] * 480 / rain_graph.size[0])))
+    image.paste(rain_graph, (0, 270))
+
 
     _waveshare.show_image(image)
 
