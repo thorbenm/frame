e02d1446af9bfc8df2f010f2e9ea28f2721dd0fc
thorben muhleder <thorbenm@me.com>
2023-07-31 14:21:48 +0200

    poc SL. change coloring behaviour

--- capture_website.py
+++ capture_website.py
@@ -3,14 +3,15 @@ from selenium.webdriver.chrome.options import Options
 from io import BytesIO
 from PIL import Image
 
-def capture_screenshot(url, wait=10):
+def capture_screenshot(url, size="1920x1080", wait=10):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument(f"--window-size={size}")
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
     driver.implicitly_wait(wait)
+
     screenshot_bytes = driver.get_screenshot_as_png()
     driver.quit()
     greyscale = Image.open(BytesIO(screenshot_bytes)).convert('L')
--- update.py
+++ update.py
@@ -10,30 +10,43 @@ DIMENSIONS = (480, 800)
 FONT_PATH = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
 
 
+def _map(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+def scale_image_values(image, scale_black, scale_white):
+    def fun(x):
+        fx = float(x)
+        fx = _map(fx, scale_black, scale_white, 0.0, 255.0)
+        fx = min(fx, 255.0)
+        fx = max(fx, 0.0)
+        return round(fx)
+    return image.point(fun, "L")
+
+
 def get_yr():
     full = capture_screenshot("https://www.yr.no/en/forecast/daily-table/2-2670879/Sweden/Stockholm/Sundbyberg%20Municipality/Sundbyberg")
     _waveshare.save_image_to_disk(full, "yr_full")
 
     current_conditions = full.crop((330, 270, full.width - 1000, full.height - 750))
-    current_conditions_arr = np.array(current_conditions).astype(float)
-    current_conditions_arr -= np.min(current_conditions_arr)
-    current_conditions_arr -= 100
-    current_conditions_arr /= np.max(current_conditions_arr)
-    current_conditions_arr *= 255.0
-    current_conditions_arr = np.clip(current_conditions_arr, 0.0, 255.0)
-    current_conditions = Image.fromarray(current_conditions_arr.astype(int))
+    current_conditions = scale_image_values(current_conditions, 100.0, 255.0)
     _waveshare.save_image_to_disk(current_conditions, "yr_current_conditions")
 
     rain_graph = full.crop((1210, 250, full.width - 360, full.height - 740))
-    rain_graph_arr = np.array(rain_graph).astype(float)
-    rain_graph_arr -= np.min(rain_graph_arr)
-    rain_graph_arr -= 25
-    rain_graph_arr /= np.max(rain_graph_arr)
-    rain_graph_arr *= 255.0
-    rain_graph_arr = np.clip(rain_graph_arr, 0.0, 255.0)
-    rain_graph = Image.fromarray(rain_graph_arr.astype(int))
+    rain_graph = scale_image_values(rain_graph, 25.0, 255.0)
     _waveshare.save_image_to_disk(rain_graph, "yr_rain_graph")
     return current_conditions, rain_graph
+
+
+def get_sl():
+    tuletorg_full = capture_screenshot("https://sl.se/?mode=departures&origName=Tuletorget+%28Sundbyberg%29&origSiteId=3515&origPlaceId=QT0xQE89VHVsZXRvcmdldCAoU3VuZGJ5YmVyZylAWD0xNzk3NDcxM0BZPTU5MzY0ODA3QFU9NzRATD0zMDAxMDM1MTVAQj0xQA%3D%3D", size="480x1100")
+    _waveshare.save_image_to_disk(tuletorg_full, "sl_tuletorg_full")
+
+    tuletorg_cropped = tuletorg_full.crop((0, 1055, 480, 1100))
+    tuletorg_cropped = scale_image_values(tuletorg_cropped, 50.0, 240.0)
+
+    _waveshare.save_image_to_disk(tuletorg_cropped, "sl_tuletorg_cropped")
+    return tuletorg_cropped
 
 
 def add_text_to_image(draw, text, size, location):
@@ -67,6 +80,8 @@ def main():
     rain_graph = rain_graph.resize((480, round(rain_graph.size[1] * 480 / rain_graph.size[0])))
     image.paste(rain_graph, (0, 270))
 
+    sl = get_sl()
+    image.paste(sl, (0, 500))
 
     _waveshare.show_image(image)
 
