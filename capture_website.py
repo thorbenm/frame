4c8c0fa62667defb554781d7ca6454376e808b08
thorben muhleder <thorbenm@me.com>
2023-08-29 11:20:47 +0200

    add more Yr and SL

--- capture_website.py
+++ capture_website.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from io import BytesIO
 from PIL import Image
 
-def capture_screenshot(url, size="1920x1080", wait=10):
+def capture_screenshot(url, size="1920x1080", wait=10, press_coordinates=None):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
@@ -11,6 +11,13 @@ def capture_screenshot(url, size="1920x1080", wait=10):
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
     driver.implicitly_wait(wait)
+
+    if press_coordinates is not None:
+        action = webdriver.common.action_chains.ActionChains(driver)
+        action.move_by_offset(*press_coordinates)
+        action.click()
+        action.perform()
+        driver.implicitly_wait(wait)
 
     screenshot_bytes = driver.get_screenshot_as_png()
     driver.quit()
--- update.py
+++ update.py
@@ -30,8 +30,10 @@ def get_yr():
     full = capture_screenshot("https://www.yr.no/en/forecast/daily-table/2-2670879/Sweden/Stockholm/Sundbyberg%20Municipality/Sundbyberg")
     _waveshare.save_image_to_disk(full, "yr_full")
 
-    current_conditions = full.crop((330, 270, full.width - 1000, full.height - 750))
-    current_conditions = image_contrast(current_conditions, 150.0, 255.0)
+    current_conditions = full.crop((330, 260, full.width - 1000, full.height - 750))
+    # for j in range(0, 255):
+    #     _waveshare.save_image_to_disk(image_contrast(current_conditions, float(j), 255.0).convert("1"), str(j)) 
+    current_conditions = image_contrast(current_conditions, 200.0, 255.0)
     _waveshare.save_image_to_disk(current_conditions, "yr_current_conditions")
 
     rain_graph = full.crop((1210, 250, full.width - 360, full.height - 740))
@@ -40,15 +42,64 @@ def get_yr():
     return current_conditions, rain_graph
 
 
-def get_sl():
-    tuletorg_full = capture_screenshot("https://sl.se/?mode=departures&origName=Tuletorget+%28Sundbyberg%29&origSiteId=3515&origPlaceId=QT0xQE89VHVsZXRvcmdldCAoU3VuZGJ5YmVyZylAWD0xNzk3NDcxM0BZPTU5MzY0ODA3QFU9NzRATD0zMDAxMDM1MTVAQj0xQA%3D%3D", size="480x1100")
-    _waveshare.save_image_to_disk(tuletorg_full, "sl_tuletorg_full")
+def get_forecast():
+    full = capture_screenshot("https://www.yr.no/en/forecast/graph/2-2670879/Sweden/Stockholm/Sundbyberg%20Municipality/Sundbyberg", size="495x1100")
+    _waveshare.save_image_to_disk(full, "yr_full2")
 
-    tuletorg_cropped = tuletorg_full.crop((0, 1055, 480, 1100))
-    tuletorg_cropped = image_contrast(tuletorg_cropped, 100.0, 240.0)
+    forecast = full.crop((0, 351, 480, 518))
+    forecast = image_contrast(forecast, 180.0, 245.0)
+    _waveshare.save_image_to_disk(forecast, "yr_forecast")
 
-    _waveshare.save_image_to_disk(tuletorg_cropped, "sl_tuletorg_cropped")
-    return tuletorg_cropped
+    return forecast
+
+
+def get_sl_tunnelbana():
+    tunnelbana_full = capture_screenshot("https://sl.se/?mode=departures&origName=Sundbybergs+station+%28Sundbyberg%29&origSiteId=9325&origPlaceId=QT0xQE89U3VuZGJ5YmVyZ3Mgc3RhdGlvbiAoU3VuZGJ5YmVyZylAWD0xNzk3MTQ5NUBZPTU5MzYwODcwQFU9NzRATD0zMDIxMDkzMjVAQj0xQA%3D%3D", size="495x1500", press_coordinates=(24, 920))
+    _waveshare.save_image_to_disk(tunnelbana_full, "sl_tunnelbana_full")
+
+    elements = list()
+    elements.append(tunnelbana_full.crop((0, 1071, 480, 1132)))
+    elements.append(tunnelbana_full.crop((0, 1140, 480, 1201)))
+    _waveshare.save_image_to_disk(elements[0], "sl_tunnelbana_0")
+    _waveshare.save_image_to_disk(elements[1], "sl_tunnelbana_1")
+
+    kungstradgarden = min(elements, key=lambda img: sum(img.crop((154, 0, 260, img.size[1])).getdata()))
+    kungstradgarden = image_contrast(kungstradgarden, 100.0, 240.0)
+    kungstradgarden.paste(255, (0, 0, 11, kungstradgarden.height))
+    _waveshare.save_image_to_disk(kungstradgarden, "sl_tunnelbana_kungstradgarden")
+
+    return kungstradgarden
+
+
+def get_sl_pendel():
+    pendel_full = capture_screenshot("https://sl.se/?mode=departures&origName=Sundbybergs+station+%28Sundbyberg%29&origSiteId=9325&origPlaceId=QT0xQE89U3VuZGJ5YmVyZ3Mgc3RhdGlvbiAoU3VuZGJ5YmVyZylAWD0xNzk3MTQ5NUBZPTU5MzYwODcwQFU9NzRATD0zMDIxMDkzMjVAQj0xQA%3D%3D", size="495x1500", press_coordinates=(308, 920))
+    _waveshare.save_image_to_disk(pendel_full, "sl_pendel_full")
+
+    elements = list()
+    elements.append(pendel_full.crop((0, 1071, 480, 1132)))
+    elements.append(pendel_full.crop((0, 1140, 480, 1201)))
+    elements.append(pendel_full.crop((0, 1209, 480, 1270)))
+    _waveshare.save_image_to_disk(elements[0], "sl_pendel_0")
+    _waveshare.save_image_to_disk(elements[1], "sl_pendel_1")
+    _waveshare.save_image_to_disk(elements[2], "sl_pendel_2")
+
+    vasterhaninge = min(elements, key=lambda img: sum(img.crop((176, 0, 260, img.size[1])).getdata()))
+    vasterhaninge = image_contrast(vasterhaninge, 100.0, 240.0)
+    vasterhaninge.paste(255, (0, 0, 11, vasterhaninge.height))
+    _waveshare.save_image_to_disk(vasterhaninge, "sl_pendel_vasterhanigne")
+
+    return vasterhaninge
+
+
+def get_sl_bus():
+    bus_full = capture_screenshot("https://sl.se/?mode=departures&origName=Tuletorget+%28Sundbyberg%29&origSiteId=3515&origPlaceId=QT0xQE89VHVsZXRvcmdldCAoU3VuZGJ5YmVyZylAWD0xNzk3NDcxM0BZPTU5MzY0ODA3QFU9NzRATD0zMDAxMDM1MTVAQj0xQA%3D%3D", size="495x1500")
+    _waveshare.save_image_to_disk(bus_full, "sl_bus_full")
+
+    bus_cropped = bus_full.crop((0, 1023, 480, 1084))
+    bus_cropped = image_contrast(bus_cropped, 100.0, 240.0)
+
+    _waveshare.save_image_to_disk(bus_cropped, "sl_bus_cropped")
+    return bus_cropped
 
 
 def add_text_to_image(draw, text, size, location):
@@ -60,33 +111,72 @@ def add_text_to_image(draw, text, size, location):
 
 
     draw.text((x, y), text, font=ImageFont.truetype(FONT_PATH, size), fill=0)
+    return text_height
 
 
-@timeout(59)
-def main():
+def generate_image():
     image = Image.new('1', DIMENSIONS, 255)
     draw = ImageDraw.Draw(image)
 
     current_date = time.strftime('%a, %Y-%-m-%-d')
     week_number = datetime.datetime.now().isocalendar()[1]
     current_date += ", Week " + str(week_number)
-    add_text_to_image(draw, current_date, 30, (-1, 10))
 
-    current_time = time.strftime('%-I:%M')
-    add_text_to_image(draw, current_time, 150, (-1, 20))
+    c = 0  # vertical cursor
+
+    c += add_text_to_image(draw, current_date, 30, (-1, c))
+    c -= 29
+
+
+    current_time = (datetime.datetime.now() + datetime.timedelta(minutes=1)).strftime('%-I:%M')
+    # its actually the time + 1 minute because we start the process early
+    c += add_text_to_image(draw, current_time, 150, (-1, c))
+
+    c += 6
+    draw.line([(0, c), (DIMENSIONS[0] - 1, c)], fill=0, width=2)
+    c += 2
 
     current_conditions, rain_graph = get_yr()
 
     current_conditions = current_conditions.resize((480, round(current_conditions.size[1] * 480 / current_conditions.size[0])))
-    image.paste(current_conditions, (0, 200))
+    image.paste(current_conditions, (0, c))
+    c += current_conditions.size[1]
 
     rain_graph = rain_graph.resize((480, round(rain_graph.size[1] * 480 / rain_graph.size[0])))
-    image.paste(rain_graph, (0, 270))
+    image.paste(rain_graph, (0, c))
+    c += rain_graph.size[1]
 
-    sl = get_sl()
-    image.paste(sl, (0, 420))
+    forecast = get_forecast()
+    image.paste(forecast, (0, c))
+    c += forecast.size[1]
 
-    _waveshare.show_image(image)
+    draw.line([(0, c), (DIMENSIONS[0] - 1, c)], fill=0, width=2)
+    c += 2
+
+    tunnelbana = get_sl_tunnelbana()
+    image.paste(tunnelbana, (0, c))
+    c += tunnelbana.size[1]
+
+    pendel = get_sl_pendel()
+    image.paste(pendel, (0, c))
+    c += pendel.size[1]
+
+    bus = get_sl_bus()
+    image.paste(bus, (0, c))
+    c += bus.size[1]
+
+
+
+    #
+    _waveshare.save_image_to_disk(image, "wip")
+    #
+
+    return image
+
+
+@timeout(59)
+def main():
+    _waveshare.show_image(generate_image())
 
 
 def try_main():
