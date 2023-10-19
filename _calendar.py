c69adf2948dfcd21e0f9c85d9eeeed1b9253a1f6
thorben muhleder <thorbenm@me.com>
2023-10-19 15:38:05 +0200

    add support for reoccuring events

--- _calendar.py
+++ _calendar.py
@@ -1,9 +1,10 @@
 import requests
 from icalendar import Calendar
 from urllib.parse import urlparse
-from datetime import datetime, timedelta, date
+from datetime import datetime, date
 from personal_data import webcal_url
 from pytz import timezone
+from dateutil.rrule import rrulestr
 
 
 def get_events():
@@ -20,7 +21,7 @@ def get_events():
 
     tz = timezone('Europe/Stockholm')
     
-    current_time = datetime.now(tz).replace(tzinfo=None)
+    current_time = datetime.now(tz)
 
     ret = list()
     
@@ -39,16 +40,38 @@ def get_events():
         elif isinstance(event_end, date):
             event_end = datetime.combine(event_end, datetime.min.time(), tz)
 
-        threshold = min(event_start.replace(hour=23, minute=59), event_end)
-        threshold = threshold.replace(tzinfo=None)
-    
-        if current_time < threshold:
-            e = lambda: None
-            e.name = event.get('summary')
-            e.name = e.name[0].capitalize() + e.name[1:]
-            e.start = event_start.replace(tzinfo=None)
-            e.end = event_end.replace(tzinfo=None)
-            ret.append(e)
+        if 'RRULE' in event:
+            # reoccuring event
+            rule = rrulestr(event['RRULE'].to_ical().decode('utf-8'), dtstart=event_start)
+            occurrences = rule.after(current_time)
+            if occurrences is not None:
+                e = lambda: None
+                e.name = event.get('summary')
+                e.name = e.name[0].capitalize() + e.name[1:]
+                e.start = occurrences.replace(tzinfo=None)
+                e.end = (occurrences + (event_end - event_start)).replace(tzinfo=None)
+
+                counter = 0
+                while occurrences is not None and counter <= 100:
+                    counter += 1
+                    occurrences = rule.after(occurrences)
+
+                if 0 < counter:
+                    e.name += " (+" + str(counter) + ")"
+
+                ret.append(e)
+        else:
+            # single event
+            threshold = min(event_start.replace(hour=23, minute=59), event_end)
+            threshold = threshold.replace(tzinfo=None)
+
+            if current_time.replace(tzinfo=None) < threshold:
+                e = lambda: None
+                e.name = event.get('summary')
+                e.name = e.name[0].capitalize() + e.name[1:]
+                e.start = event_start.replace(tzinfo=None)
+                e.end = event_end.replace(tzinfo=None)
+                ret.append(e)
 
     ret = sorted(ret, key=lambda x: x.start)
 
@@ -80,6 +103,9 @@ def convert(target_date):
     time = target_date.strftime('%-H:%M')
     if time != "0:00":
         ret += ", " + time 
+
+    if 22 < len(ret):
+        ret = ret[:-3]
 
     return ret
 
